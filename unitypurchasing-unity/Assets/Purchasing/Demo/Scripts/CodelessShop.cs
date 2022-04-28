559dcc15eed9f4f2d6ffdd3ff8bbb51094e18ee6
Mansour <mansurjavadi@gmail.com>
2022-04-28 19:39:46 +0430

    Add demo project

---
+++ unitypurchasing-unity/Assets/Purchasing/Demo/Scripts/CodelessShop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using RTLTMPro;
+using UnityEngine;
+
+public class CodelessShop : MonoBehaviour
+{
+    [SerializeField] protected Vehicle vehicle;
+    [SerializeField] protected RTLTextMeshPro consoleText;
+
+    public void UpdateStats(String productId)
+    {
+        switch (productId)
+        {
+            case "infinite_gas_monthly":
+                vehicle.SetGas(5);
+                break;
+            case "gas":
+                vehicle.Increase();
+                break;
+            case "premium":
+                vehicle.SetSkin(1);
+                break;
+        }
+    }
+
+    protected void Log(string message)
+    {
+        consoleText.text = consoleText.OriginalText + message + "\n";
+    }
+
+}
